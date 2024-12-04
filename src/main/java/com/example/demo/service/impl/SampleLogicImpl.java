4a29bcf08ccfb9f5987e86efe16a95382f558889
rotosiri-zu <hongjitufang25@gmail.com>
2024-12-04 16:35:53 +0900

    SampleLogicImpl実装クラス作成

---
+++ src/main/java/com/example/demo/service/impl/SampleLogicImpl.java
@@ -0,0 +1,12 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.BusinessLogic;
+
+public class SampleLogicImpl implements BusinessLogic {
+
+	@Override
+	public void doLigic() {
+		System.out.println("サンプルです");
+	}
+
+}
